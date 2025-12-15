900381eb9218e4e17b94ff0b6b5a45ba26e1335f
allmore0 <allmore0@gmail.com>
2025-12-15 04:05:40 -0600

    Refactor app.py to include signal handling

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 import os
+import signal
 import json
 import csv
 import requests
@@ -168,5 +169,6 @@ def submit():
 
 if __name__ == '__main__':
     app.run(debug=True, port=10000)
+
 
 
